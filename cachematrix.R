0131e8768d2a4aae1ce4c675942a9bd82af00bc5
MichaelRafferty <michael.rafferty65@gmail.com>
2015-04-20 15:58:24 -0400

    Removing Example Code and expanding comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,9 @@
-## This file contains four functions
-## 1 & 2) makeCacheMatrix and cacheSolve functions required by the assignment
-## 3 & 4) example functions from the assignment
+## This file Programming assignment 2 required functions
+### makeCacheMatrix and cacheSolve functions
+## The technique for caching data and accessing the cache are coppied from 
+### the example in the assignment
 
 ## makeCacheMatrix makes a 'matrix' capable of storing its value and inverse
-
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
     
@@ -26,13 +26,14 @@ makeCacheMatrix <- function(x = matrix()) {
     list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-## cacheSolve checks for a cache'd inverse
+## cacheSolve checks for a cached inverse
 ## if a cached inverse doesn't exist the inverse is calculated and cached
 cacheSolve <- function(x, ...) {
+    ### get cached inverse
     inv <- x$getinv()
     ### if inv exists then return
     if(!is.null(inv)) {
-        message("getting cached data")
+        message("retrieving cached data")
         return(inv)
     }
     
@@ -43,30 +44,4 @@ cacheSolve <- function(x, ...) {
     
     ### return calculated value
     inv
-}
-
-
-## example functions from the asssignment
-makeVector <- function(x = numeric()) {
-    m <- NULL
-    set <- function(y) { 
-        x<<-y
-        m<<-NULL
-    }
-    get <- function() x
-    setmean <- function(mean) m <<-mean
-    getmean <- function() m
-    list(set =set, get=get, setmean=setmean, getmean=getmean)
-}
-
-cachemean <- function(x, ...) {
-    m <- x$getmean()
-    if(!is.null(m)) {
-        message("getting cached data")
-        return(m)
-    }
-    data <- x$get()
-    m <- mean(data, ...)
-    x$setmean(m)
-    m
 }
